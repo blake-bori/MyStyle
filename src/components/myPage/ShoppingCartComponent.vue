a5f526b2f42f58cec14e41c44203537cb9a8a369
seyeon2 <bb_bb_bb_53@naver.com>
2021-10-28 21:31:07 +0900

    Add Recommand Slider

--- src/components/myPage/ShoppingCartComponent.vue
+++ src/components/myPage/ShoppingCartComponent.vue
@@ -18,6 +18,8 @@
             </div>
         </section>
         <!-- Breadcrumb Section End -->
+
+        <!-- Image Slider 1 Start -->
         <div class="topbottom_text">
         <h5> Top</h5>
         </div>
@@ -74,8 +76,9 @@
                 </div>
             </div>
         </section>
-       
-        <!-- Image Slider End -->
+        <!-- Image Slider 1 End -->
+
+        <!-- Image Slider 2 Start -->
         <div class="topbottom_text">
         <h5> Bottom</h5>
         </div>
@@ -132,9 +135,71 @@
                 </div>
             </div>
         </section>
+        <!-- Image Slider 2 End -->
+
+         <!-- Modeling Canvas -->
         <div style="text-align:center; ">
             <canvas class="col-lg-8 col-md-6" style="border: 1px solid black; width:600px; height:600px"></canvas>
         </div>
+
+        <!-- Image Slider 3 Start -->
+        <div class="topbottom_text">
+        <h5> Reccomand</h5>
+        </div>
+        <section class="spad3">
+            <div class="slider">
+                <div class="container">
+                    <div class="row slick_slide">
+                        <div class="col-3">
+                            <div class="cloth">
+                                <img src="img/shopping-cart/cart-1.jpg" alt="" class="img-fluid">
+                                <h5>Cloth 1</h5>
+                                <p>cloth 1</p>
+                            </div>
+                        </div>
+
+                        <div class="col-3">
+                            <div class="cloth">
+                                <img src="img/shopping-cart/cart-2.jpg" alt="" class="img-fluid">
+                                <h5>Cloth 2</h5>
+                                <p>cloth 2</p>
+                            </div>
+                        </div>
+
+                        <div class="col-3">
+                            <div class="cloth">
+                                <img src="img/shopping-cart/cart-3.jpg" alt="" class="img-fluid">
+                                <h5>Cloth 3</h5>
+                                <p>cloth 3</p>
+                            </div>
+                        </div>
+
+                        <div class="col-3">
+                            <div class="cloth">
+                                <img src="img/shopping-cart/cart-4.jpg" alt="" class="img-fluid">
+                                <h5>Cloth 4</h5>
+                                <p>cloth 4</p>
+                            </div>
+                        </div>
+                        <div class="col-3">
+                            <div class="cloth">
+                                <img src="img/shopping-cart/cart-4.jpg" alt="" class="img-fluid">
+                                <h5>Cloth 4</h5>
+                                <p>cloth 4</p>
+                            </div>
+                        </div>
+                        <div class="col-3">
+                            <div class="cloth">
+                                <img src="img/shopping-cart/cart-4.jpg" alt="" class="img-fluid">
+                                <h5>Cloth 4</h5>
+                                <p>cloth 4</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Image Slider 3 End -->
     </div>
 </template>
 
