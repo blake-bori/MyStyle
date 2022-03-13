e551f50a1980c801e4b80c5e4176677d74ff58f2
seyeon2 <bb_bb_bb_53@naver.com>
2022-03-13 16:18:55 +0900

    Code re-factoring and add comments

--- FE/src/components/home/HomeComponent.vue
+++ FE/src/components/home/HomeComponent.vue
@@ -26,9 +26,9 @@
                             <div class="col-xl-5 col-lg-7 col-md-8">
                                 <div class="hero__text">
                                     <!-- 신체 모델링관련 홍보로 신체 사이즈 측정 유도 -->
-                                    <h6>프로모션 - 2 주제</h6>
-                                    <h2>프로모션 - 2 제목</h2>
-                                    <p>프로모션에 관한 내용 어쩌구 저쩌구.... 어쩌구 저쩌구....</p>
+                                    <h6>마이스타일만의 특별한 두번째 기능</h6>
+                                    <h2>찜한 옷 입어보기</h2>
+                                    <p>자신만의 마네킹에 마음에 드는 옷을 적용해 보세요</p>
                                     <a href="#" class="primary-btn">Shop now <span class="arrow_right"></span></a>
                                     <div class="hero__social">
                                         <a href="#"><i class="fa fa-facebook"></i></a>
@@ -105,9 +105,6 @@
                 </div>
                 <div class="row product__filter">
                     <!-- new-arrivals, hot-sales를 구분하기 위한 class -->
-                    <!-- <div class="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix new-arrivals"> -->
-                    <!-- <div class="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix hot-sales"> -->
-                    <!-- 할일 : 메인 페이지 필터링 적용 되는지 서버 연동 시도해보기 -->
                     <div
                         v-for="product in homeProductList"
                         :key="product.idx"
@@ -131,220 +128,10 @@
                             </div>
                         </div>
                     </div>
-
-                    <!-- <div class="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix new-arrivals">
-                        <div class="product__item">
-                            <div class="product__item__pic set-bg" data-setbg="img/product/product-1.jpg">
-                                <span class="label">New</span>
-                                <ul class="product__hover">
-                                    <li>
-                                        <a href="./detail"><img src="img/icon/search.png" alt="" /><span>상세정보</span></a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="product__item__text">
-                                <h6>Piqué Biker Jacket</h6>
-                                <a href="./detail" class="add-cart">상세정보</a>
-                                <h5>$67.24</h5>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix hot-sales">
-                        <div class="product__item">
-                            <div class="product__item__pic set-bg" data-setbg="img/product/product-2.jpg">
-                                <ul class="product__hover">
-                                    <li>
-                                        <a href="./detail"><img src="img/icon/search.png" alt="" /><span>상세정보</span></a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="product__item__text">
-                                <h6>Piqué Biker Jacket</h6>
-                                <a href="#" class="add-cart">상세정보</a>
-                                <h5>$67.24</h5>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix new-arrivals">
-                        <div class="product__item sale">
-                            <div class="product__item__pic set-bg" data-setbg="img/product/product-3.jpg">
-                                <span class="label">Sale</span>
-                                <ul class="product__hover">
-                                    <li>
-                                        <a href="./detail"><img src="img/icon/search.png" alt="" /><span>상세정보</span></a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="product__item__text">
-                                <h6>Multi-pocket Chest Bag</h6>
-                                <a href="#" class="add-cart">상세정보</a>
-                                <h5>$43.48</h5>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix hot-sales">
-                        <div class="product__item">
-                            <div class="product__item__pic set-bg" data-setbg="img/product/product-4.jpg">
-                                <ul class="product__hover">
-                                    <li>
-                                        <a href="./detail"><img src="img/icon/search.png" alt="" /><span>상세정보</span></a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="product__item__text">
-                                <h6>Diagonal Textured Cap</h6>
-                                <a href="#" class="add-cart">상세정보</a>
-                                <h5>$60.9</h5>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix new-arrivals">
-                        <div class="product__item">
-                            <div class="product__item__pic set-bg" data-setbg="img/product/product-5.jpg">
-                                <ul class="product__hover">
-                                    <li>
-                                        <a href="./detail"><img src="img/icon/search.png" alt="" /><span>상세정보</span></a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="product__item__text">
-                                <h6>Lether Backpack</h6>
-                                <a href="#" class="add-cart">상세정보</a>
-                                <h5>$31.37</h5>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix hot-sales">
-                        <div class="product__item sale">
-                            <div class="product__item__pic set-bg" data-setbg="img/product/product-6.jpg">
-                                <span class="label">Sale</span>
-                                <ul class="product__hover">
-                                    <li>
-                                        <a href="./detail"><img src="img/icon/search.png" alt="" /><span>상세정보</span></a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="product__item__text">
-                                <h6>Ankle Boots</h6>
-                                <a href="#" class="add-cart">상세정보</a>
-                                <h5>$98.49</h5>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix new-arrivals">
-                        <div class="product__item">
-                            <div class="product__item__pic set-bg" data-setbg="img/product/product-7.jpg">
-                                <ul class="product__hover">
-                                    <li>
-                                        <a href="./detail"><img src="img/icon/search.png" alt="" /><span>상세정보</span></a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="product__item__text">
-                                <h6>T-shirt Contrast Pocket</h6>
-                                <a href="#" class="add-cart">상세정보</a>
-                                <h5>$49.66</h5>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix hot-sales">
-                        <div class="product__item">
-                            <div class="product__item__pic set-bg" data-setbg="img/product/product-8.jpg">
-                                <ul class="product__hover">
-                                    <li>
-                                        <a href="./detail"><img src="img/icon/search.png" alt="" /><span>상세정보</span></a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="product__item__text">
-                                <h6>Basic Flowing Scarf</h6>
-                                <a href="#" class="add-cart">상세정보</a>
-                                <h5>$26.28</h5>
-                            </div>
-                        </div>
-                    </div> -->
                 </div>
             </div>
         </section>
         <!-- Product Section End -->
-
-        <!-- Categories Section Begin -->
-        <!-- <section class="categories spad">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-3">
-                        <div class="categories__text">
-                            <h2>
-                                Clothings Hot <br />
-                                <span>Shoe Collection</span> <br />
-                                Accessories
-                            </h2>
-                        </div>
-                    </div>
-                    <div class="col-lg-4">
-                        <div class="categories__hot__deal">
-                            <img src="img/product-sale.png" alt="" />
-                            <div class="hot__deal__sticker">
-                                <span>Sale Of</span>
-                                <h5>$29.99</h5>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 offset-lg-1">
-                        <div class="categories__deal__countdown">
-                            <span>Deal Of The Week</span>
-                            <h2>Multi-pocket Chest Bag Black</h2>
-                            <div class="categories__deal__countdown__timer" id="countdown">
-                                <div class="cd-item">
-                                    <span>3</span>
-                                    <p>Days</p>
-                                </div>
-                                <div class="cd-item">
-                                    <span>1</span>
-                                    <p>Hours</p>
-                                </div>
-                                <div class="cd-item">
-                                    <span>50</span>
-                                    <p>Minutes</p>
-                                </div>
-                                <div class="cd-item">
-                                    <span>18</span>
-                                    <p>Seconds</p>
-                                </div>
-                            </div>
-                            <a href="#" class="primary-btn">Shop now</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section> -->
-        <!-- Categories Section End -->
-
-        <!-- Instagram Section Begin -->
-        <!-- <section class="instagram spad">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-8">
-                        <div class="instagram__pic">
-                            <div class="instagram__pic__item set-bg" data-setbg="img/instagram/instagram-1.jpg"></div>
-                            <div class="instagram__pic__item set-bg" data-setbg="img/instagram/instagram-2.jpg"></div>
-                            <div class="instagram__pic__item set-bg" data-setbg="img/instagram/instagram-3.jpg"></div>
-                            <div class="instagram__pic__item set-bg" data-setbg="img/instagram/instagram-4.jpg"></div>
-                            <div class="instagram__pic__item set-bg" data-setbg="img/instagram/instagram-5.jpg"></div>
-                            <div class="instagram__pic__item set-bg" data-setbg="img/instagram/instagram-6.jpg"></div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4">
-                        <div class="instagram__text">
-                            <h2>Instagram</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                            <h3>#Male_Fashion</h3>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section> -->
-        <!-- Instagram Section End -->
 
         <!-- 팀원 소개 컨텐츠 -->
         <!-- Latest Blog Section Begin -->
