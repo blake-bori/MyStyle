c952134a8793d121e62613fc7bc1d492ea60e0c9
seyeon2 <bb_bb_bb_53@naver.com>
2022-03-25 16:42:22 +0900

    add comment

--- FE/src/components/shop/ModelDetailShow.vue
+++ FE/src/components/shop/ModelDetailShow.vue
@@ -1,4 +1,5 @@
 <template>
+    <!-- gltf 실행 -->
     <model-gltf
         :src="url"
         :controlsOptions="{
@@ -11,6 +12,7 @@
     </model-gltf>
 </template>
 <script>
+// gilt 실행을 위한 라이브러리 임포트
 import { ModelGltf } from "vue-3d-model";
 export default {
     //example of object
@@ -22,6 +24,7 @@ export default {
     },
     computed: {
         url() {
+            // 선택된 상품이 있으면 해당 상품의 모델을 가져옴
             if (this.$store.state.product.productData != null) {
                 return this.$store.state.product.productData.clothes.model;
             } else {
