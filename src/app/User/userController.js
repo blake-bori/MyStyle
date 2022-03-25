124a734294b19d02624bbbb9f96bd26c3638c5f0
임수이 <80459739+SY-Mina@users.noreply.github.com>
2022-03-25 22:59:22 +0900

    Comment UserController

--- src/app/User/userController.js
+++ src/app/User/userController.js
@@ -9,12 +9,9 @@ const {spawn} = require('child_process');
 const { PythonShell } = require("python-shell");
 
 const s3Client = require("../../../config/s3");
-// const multer  = require('multer');
 var formidable = require('formidable')
 var fs = require('fs');
 const AWS = require('aws-sdk');
-//const extract = require('extract-zip')
-//var JSZip = require("jszip");
 var unzipper = require("unzipper")
 
 /**
@@ -26,9 +23,10 @@ var unzipper = require("unzipper")
 exports.getTest = async function (req, res) {
     return res.send(response(baseResponse.SUCCESS))
 }
+
 /**
  * API No.
- * API Name : 파이썬 파일 실행 API
+ * API Name : 파이썬 파일 실행 및 S3 업로드 테스트  API
  * [POST] /app/users/test
  */
 exports.getPyTest = async function (req, res) {
@@ -127,7 +125,7 @@ exports.postUsers = async function (req, res) {
     var file = req.files.uploadFile;
     console.log(file)
 
-    //가져온 zip파일 서버에 저장.
+    // 받아온 zip파일 서버에 저장.
     fs.writeFile(file.name, file.data, (err)=>{
         if (err) {
             console.log(err)
@@ -135,7 +133,7 @@ exports.postUsers = async function (req, res) {
         }
     });
 
-    // 회원가입처리
+    // 유저 회원가입 처리
     const signUpResponseIdx = await userService.createUser(
         userId, password, userName
     );
@@ -150,35 +148,12 @@ exports.postUsers = async function (req, res) {
     var target = 'user' + signUpResponseIdx;
     const path = './user'+signUpResponseIdx;
 
-    //zip 압축 해제 후 서버 내 유저 디렉토리에 저장
+    // zip 압축 해제 후 서버 내 유저 디렉토리에 저장
     fs.createReadStream(file.name)
         .pipe(unzipper.Extract({path:target}));
 
-    //유저의 모델로 쉽게 인식할 수 있게 파일 이름 바꾸기
-    //회원가입이니까 파일 하나 업로드하면 될듯
 
-    // fs.readdir(path, (err, files) => {
-    //     console.log(files)
-    //
-    //     var i=0;
-    //     files.forEach(file => {
-    //         const asis = path + '/'+ file;
-    //         const tobe = path + '/'+ 'human.obj';
-    //         fs.rename(asis, tobe, function (err) {
-    //             if (err){
-    //                 console.log("rename error : " + asis + " => " + tobe)
-    //                 console.log(err)
-    //             }else{
-    //                 console.log('renamed complete : ' + asis + " => " + tobe);
-    //             }
-    //         });
-    //         i++
-    //     })
-    //
-    // })
-
-
-    // 서버 용량 아끼기 위해 zip 파일 삭제해놓기
+    // 서버 용량의 효율을 위해 zip 파일 삭제해놓기
     fs.unlink(file.name, err => {
         if(err){
             console.log("파일 삭제 Error 발생");
@@ -186,148 +161,9 @@ exports.postUsers = async function (req, res) {
         }
     });
 
-
-    //S3 업로드
-    // var bucketName = 'usermodel'
-    //
-    // const s3 = new AWS.S3({
-    //     accessKeyId: s3Client.accessid,
-    //     secretAccessKey: s3Client.secret,
-    //     region: 'ap-northeast-2',
-    //     Bucket: bucketName
-    // });
-    //
-    // const params = {
-    //     Bucket: 'usermodel',
-    //     Key: target,
-    //     Body: files
-    // };
-    // s3.upload(params, function (err, data) {
-    //     if (err) {
-    //         //throw err;
-    //         console.log('error')
-    //         console.log(err)
-    //     } else {
-    //         console.log(`File uploaded successfully.`);
-    //         console.log(data.Location)
-    //         usermodelUrl = data.Location
-    //         const updateUsermodel = userService.insertUsermodel(
-    //             usermodelUrl, signUpResponseIdx
-    //         );
-    //         return res.send(updateUsermodel);
-    //     }
-    // });
-
-    // try {
-    //     await extract(file, { dir: target })
-    //     console.log('Extraction complete')
-    // } catch (err) {
-    //     // handle any errors
-    //     console.log(err)
-    // }
-
-    // console.log(s3Client.accessid)
-    // console.log(s3Client.secret)
-
-
     // signUpResponse 값을 json으로 전달
     //return res.send(signUpResponse);
     return res.send(response(baseResponse.SUCCESS));
-};
-
-/**
- * API No. 1.5
- * API Name : 회원가입시 파일 업로드 API
- * [POST] /app/users/images
- */
-exports.postUserImage = async function (req, res) {
-
-    /**
-     * Body: bodyModel
-     */
-    var file = req.files.uploadFile;
-    console.log(file)
-
-    if (!req.files) {
-        res.send({
-            status: false,
-            message: '파일 업로드 실패'
-        });
-    } else {
-        var file = req.files.uploadFile;
-
-        console.log(s3Client.accessid)
-        console.log(s3Client.secret)
-        var bucketName = 'usermodel'
-
-        const s3 = new AWS.S3({
-            accessKeyId: s3Client.accessid,
-            secretAccessKey: s3Client.secret,
-            region: 'ap-northeast-2',
-            Bucket: bucketName
-        });
-        // s3.createBucket(function () {
-        //     var params = {
-        //         Bucket: bucketName
-        //         Key: file.file.originalFilename,
-        //         Body: file.data
-        //     }
-        // })
-
-        const params = {
-            Bucket: 'usermodel',
-            Key: file.name,
-            Body: file.data
-        };
-        s3.upload(params, function(err, data) {
-            if (err) {
-                //throw err;
-                console.log('error')
-                console.log(err)
-            } else {
-                console.log(`File uploaded successfully.`);
-                console.log(data.Location)
-            }
-
-        });
-
-    }
-
-    return res.send(response(baseResponse.SUCCESS));
-
-
-};
-
-/**
- * API No. 2
- * API Name : 회원가입시 신체 입력 API
- * [POST] /app/users/my-size
- */
-exports.postUserSize = async function (req, res) {
-
-    /**
-     * Body: userId, password, password2
-     */
-    const {
-        userIdx, gender, height, weight, proportion, muscle
-    } = req.body;
-
-
-    // 빈 값 체크
-    if (!userIdx)
-        return res.send(response(baseResponse.USER_USERID_EMPTY));
-
-    const userRows = await userProvider.userByUserIdxCheck(userIdx);
-    console.log(userRows)
-    if (userRows.length < 1)
-        return res.send(response(baseResponse.USER_USERID_NOT_EXIST));
-
-    const signUpResponse = await userService.createUserSize(
-        userIdx, gender, height, weight, proportion, muscle
-    );
-
-    // signUpResponse 값을 json으로 전달
-    return res.send(signUpResponse);
 };
 
 /**
@@ -341,9 +177,10 @@ exports.getUserHeart = async function (req, res) {
      * Path Variable: userIdx
      */
     const userIdx = req.params.userIdx;
-    // errResponse 전달
+    // 빈 값 처리
     if (!userIdx) return res.send(errResponse(baseResponse.USER_USERID_EMPTY));
 
+    // 존재하지 않는 유저인지 확인
     const userRows = await userProvider.userByUserIdxCheck(userIdx);
     console.log(userRows)
     if (userRows.length < 1)
@@ -364,6 +201,8 @@ exports.getUsers = async function (req, res) {
      * Query String: email
      */
     const userIdx = req.params.userIdx;
+    
+    //존재하지 않는 유저인지 확인
     const userRows = await userProvider.userByUserIdxCheck(userIdx);
     console.log(userRows)
     if (userRows.length < 1)
@@ -385,14 +224,16 @@ exports.getUserById = async function (req, res) {
      * Path Variable: userId
      */
     const userId = req.params.userId;
-    // errResponse 전달
+    // 빈 값 처리
     if (!userId) return res.send(errResponse(baseResponse.USER_USERID_EMPTY));
+    
+    //존재하지 않는 유저인지 확인
     const userRows = await userProvider.userByUserIdxCheck(userId);
     console.log(userRows)
     if (userRows.length < 1)
         return res.send(response(baseResponse.USER_USERID_NOT_EXIST));
 
-    // userId를 통한 유저 검색 함수 호출 및 결과 저장
+    // userId를 통한 유저 검색
     const userByUserId = await userProvider.retrieveUser(userId);
     return res.send(response(baseResponse.SUCCESS, userByUserId));
 };
@@ -424,13 +265,14 @@ exports.patchUsers = async function (req, res) {
         userIdx, userName
     } = req.body;
 
-
+    // 빈 값 처리
     if (!userIdx) return res.send(errResponse(baseResponse.USER_USERID_EMPTY));
+    //존재하지 않는 유저인지 확인
     const userRows = await userProvider.userByUserIdxCheck(userIdx);
     console.log(userRows)
     if (userRows.length < 1)
         return res.send(response(baseResponse.USER_USERID_NOT_EXIST));
-
+    // 빈 값 처리
     if (!userName) return res.send(errResponse(baseResponse.USER_NICKNAME_EMPTY));
     else {
         const editUserInfo = await userService.editUser(userIdx, userName)
@@ -439,12 +281,3 @@ exports.patchUsers = async function (req, res) {
 
 };
 
-// JWT 이 후 주차에 다룰 내용
-/** JWT 토큰 검증 API
- * [GET] /app/auto-login
- */
-exports.check = async function (req, res) {
-    const userIdResult = req.verifiedToken.userId;
-    console.log(userIdResult);
-    return res.send(response(baseResponse.TOKEN_VERIFICATION_SUCCESS));
-};
